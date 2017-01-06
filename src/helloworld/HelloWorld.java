34d1e1b5e0078562b475265531a06d58a9e9b9bc
Destiny <Destiny@172.21.7.66>
2017-01-06 13:13:41 +0100

    Experiment

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ggggg");
+        System.out.println("aaaaaa");
     }
     
 }
